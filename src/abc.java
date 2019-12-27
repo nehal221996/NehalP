bf8d798829645e993a51711c018f3afbbff6c125
Nehal Patil <patilnv@gmail.com>
2019-12-26 21:41:43 -0800

    print hello world.....

--- src/abc.java
+++ src/abc.java
@@ -1,7 +1,9 @@
 
 public class abc 
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println();
+		System.out.println("hello world00");
 	}
 }
